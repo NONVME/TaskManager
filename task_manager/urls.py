4356e982665b7cb21e2691a23a042ee833add48a
Chashin Efim <69835249+NONVME@users.noreply.github.com>
2021-03-10 11:43:07 +0000

    refactor: move the 'main' application in to the scope

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -3,5 +3,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('task_manager.main.urls')),
+    path('', include('main.urls')),
 ]
